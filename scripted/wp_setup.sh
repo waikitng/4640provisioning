e0256a852819e98d5b3e74680684acad46f4a348
waikit <waikitng64@gmail.com>
2019-01-30 13:01:25 -0800

    started working on wp_setup.sh

--- scripted/wp_setup.sh
+++ scripted/wp_setup.sh
@@ -1,2 +1,21 @@
 setenforce 0
 sed -r -i 's/SELINUX=(enforcing|disabled)/SELINUX=permissive/' /etc/selinux/config
+
+yum install -y @core epel-release vim git tcpdump nmap-ncat curl
+
+yum update -y
+
+firewall-cmd --zone=public --add-port=22/tcp --permanent
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --zone=public --add-port=443/tcp --permanent
+
+firewall-cmd --zone=public --list-all
+
+wget https://4640.acit.site/code/ssh_setup/acit_admin_id_rsa.pub --user=student --password=w1nt3r2019
+
+yum install -y nginx
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo sytemctl status nginx
+

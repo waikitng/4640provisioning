0dca2b80f1c5e3c4384d34f2f42712a2466903dd
waikit <waikitng64@gmail.com>
2019-02-08 21:37:12 -0800

    worked on wp_setup

--- scripted/wp_setup.sh
+++ scripted/wp_setup.sh
@@ -1,32 +1,40 @@
-#assume for this script that you are root user
+sed -i '4s/.*/BOOTPROTO=static' /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo "IPADDR=192.168.254.10" >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo "PREFIX=24" >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo "GATEWAY=192.168.254.10" >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo "NETMASK=255.255.255.0" >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo "192.168.254.10/24 wp.snp.acit" >> /etc/hosts
+systemctl restart network
 
-useradd admin -p P@ssw0rd
+yum install wget
+wget https://4640.acit.site/code/ssh_setup/acit_admin_id_rsa.pub --user=student --password=w1nt3r2019
+
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+
+sudo useradd admin -p P@ssw0rd
+sudo gpasswd -a admin wheel
+cp acit_admin_id_rsa.pub ~/.ssh/authorized_keys
 
 setenforce 0
 sed -r -i 's/SELINUX=(enforcing|disabled)/SELINUX=permissive/' /etc/selinux/config
 
-yum install -y @core epel-release vim git tcpdump nmap-ncat curl wget
-
+yum install -y @core epel-release vim git tcpdump nmap-ncat curl 
 yum update -y
 
 firewall-cmd --zone=public --add-port=22/tcp --permanent
 firewall-cmd --zone=public --add-port=80/tcp --permanent
 firewall-cmd --zone=public --add-port=443/tcp --permanent
-
 firewall-cmd --zone=public --list-all
 
-wget https://4640.acit.site/code/ssh_setup/acit_admin_id_rsa.pub --user=student --password=w1nt3r2019
-
 yum install -y nginx
-
 systemctl start nginx
 systemctl enable nginx
 systemctl status nginx 
 
-#lines to verify operation
+curl http://localhost:80
 
 yum install -y mariadb-server mariadb
-
 systemctl start mariadb
 
 echo "UPDATE mysql.user SET Password=PASSWORD('P@ssw0rd') WHERE User='root';" > mariadb_security_config.sql
@@ -47,9 +55,26 @@ sed -i '31s/.*/listen.owner=nobody' /etc/php-fpm.d/www.conf
 sed -i '32s/.*/listen.group=nobody' /etc/php-fpm.d/www.conf
 sed -i '39s/.*/user=nginx' /etc/php-fpm.d/www.conf
 sed -i '41s/.*/group=nginx' /etc/php-fpm.d/www.conf
+sed -i '12s:.*:listen = /var/run/php-fpm/php-fpm.sock:' /etc/php-fpm.d/www.conf
 
 systemctl start php-fpm
 systemctl enable php-fpm
+
+sed -i '44s/.*/index index.php index.html index.htm;/' /etc/nginx/nginx.conf
+sed -i '57s/.*//' /etc/nginx/nginx.conf
+sed -i '89s/.*//' /etc/nginx/nginx.conf
+echo "location ~ \.php$ {" >> /etc/nginx/nginx.conf
+echo 'try_files $uri =404;' >> /etc/nginx/nginx.conf
+echo "fastcgi_pass unix:/var/run/php-fpm/php-fpm.sock;" >> /etc/nginx/nginx.conf
+echo "fastcgi_index index.php;" >> /etc/nginx/nginx.conf
+echo 'fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;' >> /etc/nginx/nginx.conf
+echo "include fastcgi_params;" >> /etc/nginx/nginx.conf
+echo "}" >> /etc/nginx/nginx.conf
+echo "}" >> /etc/nginx/nginx.conf
+echo "}" >> /etc/nginx/nginx.conf
+
+echo "<?php phpinfo(); ?>" > /usr/share/nginx/html/info.php
+systemctl restart nginx
 
 echo "CREATE DATABASE wordpress;" > wp_mariadb_config.sql
 echo "CREATE USER wordpress_user@localhost IDENTIFIED BY 'P@ssw0rd';" >> wp_mariadb_config.sql
@@ -58,18 +83,19 @@ echo "FLUSH PRIVILEGES;" >> wp_mariadb_config.sql
 
 mysql -u root < wp_mariadb_config.sql
 
-echo "<?php phpinfo(); ?>" > /usr/share/nginx/html/info.php
-
-systemctl restart nginx
 
 wget https://wordpress.org/latest.tar.gz
-
 tar xzvf latest.tar.gz
-
 cp wordpress/wp-config-sample.php wordpress/wp-config.php
 
-rsync -avP wordpress/ /usr/share/nginx/html/
+sed -i "23s/.*/define('DB_NAME', 'wordpress');/" wordpress/wp-config.php
+sed -i "26s/.*/define('DB_USER', 'wordpress_user');/" wordpress/wp-config.php
+sed -i "29s/.*/define('DB_PASSWORD', 'P@ssw0rd');/" wordpress/wp-config.php 
 
-mkdir /usr/share/nginx/html/wp-content/uploads
+yum install rsync 
 
-chown -R admin:nginx /usr/share/nginx/html/*
+sudo rsync -avP wordpress/ /usr/share/nginx/html/
+
+sudo mkdir /usr/share/nginx/html/wp-content/uploads
+
+sudo chown -R admin:nginx /usr/share/nginx/html/*

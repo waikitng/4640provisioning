4679e3d9a6bda0f9f40193fd3933cd4a406c123b
waikit <waikitng64@gmail.com>
2019-04-17 00:26:02 -0700

    ansible_with_roles work

--- ansible_with_roles/provisioning/wp_setup_files/wp_mariadb_config.sql
+++
@@ -1,7 +0,0 @@
-## Wordpress Database Setup
-CREATE DATABASE wordpress;
-CREATE USER wordpress_user@localhost IDENTIFIED BY 'P@ssw0rd';
-GRANT ALL PRIVILEGES ON wordpress.* TO wordpress_user@localhost;
-
-# Reload privilege tables
-FLUSH PRIVILEGES;
